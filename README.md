8aa961da7e14d6e05c780287614ae9ec8375ddac
9956799@gmail.com <Qqq_12345>
2021-11-05 21:42:55 +0200

    --------------------- 	@Bean 	public OpenAPI customOpenApi(@Value("${application-description}")String appDescription, 	                             @Value("${application-version}")String appVersion
    ------------------
    README.md
    Swagger. REST APIs Documentation\
    [openapi.json](openapi.json)

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-README.md
+README.md\
 OctiPod BACKEND
 
-Technologies
-Java 8
-Spring Boot
-Swagger, Lombok
-PostgreSQL, Liquibase
-Maven, Docker
+Technologies\
+- Java 8
+- Spring Boot
+- Swagger, Lombok
+- PostgreSQL, Liquibase
+- Maven, Docker
 
-Requirements
-• JDK 15+
-• Any IDE of your choice (Lombok plugin required). For IntelliJ IDEA: Build, Execution, Deployment -> Compiler -> Annotation processors -> Enable annotation processing
-• PostgreSQL
-• Docker 19+
+Requirements\
+- JDK 8
+- Any IDE of your choice (Lombok plugin required). 
+  For IntelliJ IDEA: Build, Execution, Deployment -> Compiler -> Annotation processors -> Enable annotation processing
+- PostgreSQL
+- Docker 19+
 
 [comment]: <> (Database &#40;PostgreSQL, MySQL&#41;)
 
@@ -20,14 +21,15 @@ Requirements
 
 [comment]: <> (• docker run --name gtasearch-stage-postgresql --restart=always -p 5442:5432 -e -e -e -d postgres:13)
 
-Build Application
-• Navigate to project directory
-• mvn clean install -Plocal I -Pdev I -Pstage I -Pprod
+Build Application\
+- Navigate to project directory
+- mvn clean install -Plocal I -Pdev I -Pstage I -Pprod
 
-Run Application
-• Navigate to project directory
-• java -jar target/gtasearch-backend-1.O.O.jar
+Run Application\
+- Navigate to project directory
+- java -jar target/gtasearch-backend-1.O.O.jar
 
-[comment]: <> (Swagger. REST APIs Documentation)
+Swagger. REST APIs Documentation\
+[openapi.json](openapi.json)
 
-[comment]: <> (• Swagger IJI endpoint: http://localhost:8080/swagger-ui.html)
+[Swagger IJI endpoint](http://localhost:8080/swagger-ui.html)
--- src/main/java/com/osdb/octipod/OctiPodApplication.java
+++ src/main/java/com/osdb/octipod/OctiPodApplication.java
@@ -1,7 +1,17 @@
 package com.osdb.octipod;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class OctiPodApplication {
@@ -14,22 +24,26 @@ public class OctiPodApplication {
 //				.pathsToMatch("/users/**")
 //				.build();
 //	}
-//
-//	@Bean
-//	public OpenAPI customOpenApi(@Value("${application-description}")String appDescription,
-//	                             @Value("${application-version}")String appVersion) {
-//		return new OpenAPI().info(new Info().title("Application API")
-//						.version(appVersion)
-//						.description(appDescription)
-//						.license(new License().name("Apache 2.0")
-//								.url("http://springdoc.org"))
-//						.contact(new Contact().name("username")
-//								.email("test@gmail.com")))
-//				.servers(List.of(new Server().url("http://localhost:8080")
-//								.description("Dev service"),
-//						new Server().url("http://localhost:8082")
-//								.description("Beta service")));
-//	}
+
+	@Bean
+	public OpenAPI customOpenApi(@Value("${application-description}")String appDescription,
+	                             @Value("${application-version}")String appVersion
+	                             //,@Value("${springdoc.version}") String av
+	) {
+		return new OpenAPI().info(new Info().title("Application API-------")
+						.version(appVersion)
+						.description(appDescription)
+						.license(new License().name("Apache 2.0----------")
+								.url("http://springdoc.org---------"))
+						.contact(new Contact().name("username-------")
+								.email("test@gmail.com--------")))
+				.servers(Arrays.asList(
+						//List.of(
+							new Server().url("http://localhost:8080").description("Dev service-------"),
+							new Server().url("http://localhost:8082").description("Beta service-----------")
+						)
+				);
+	}
 
 
 	public static void main(String[] args) {
