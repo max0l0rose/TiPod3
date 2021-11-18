d935c6deff50a99653db020c21256f11bbab864e
Davyd Berchiyan <davydberchiyan@MacBook-Pro-Davyd-2.local>
2021-11-18 13:46:12 +0200

    OPOD-3 - Update Spring Docs configurations.

---
+++ src/main/java/com/osdb/octipod/configuration/SpringDocConfig.java
@@ -0,0 +1,52 @@
+package com.osdb.octipod.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Configuration
+@FieldDefaults(level = PRIVATE)
+public class SpringDocConfig {
+
+	@Value("${app.title}")
+	String appTitle;
+
+	@Value("${app.description}")
+	String appDesc;
+
+	@Value("${app.version}")
+	String appVersion;
+
+	@Value("${app.url}")
+	String appURL;
+
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI().info(
+				new Info()
+						.title(appTitle)
+						.version(appVersion)
+						.description(appDesc)
+						.license(
+								new License()
+										.name("Apache 2.0")
+										.url("http://springdoc.org")
+						)
+						.contact(
+								new Contact()
+										.name("admin")
+										.email("admin@octipod.com")
+						))
+				.servers(Collections.singletonList(new Server().url(appURL)));
+	}
+}
